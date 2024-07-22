3ae4020a076e508ef9c10bfb4a8fbd1e85495b97
Ali-734 <ali2004mah@mail.ru>
2024-07-22 17:25:31 +0300

    new2

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -8,35 +8,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequestMapping("/cars")
 public class CarController {
 
-    private List<Car> carList;
-    private CarService carService;
+    private final CarService carService;
 
     public CarController(CarService carService) {
         this.carService = carService;
-        this.carList = new ArrayList<>();
-        this.carList.add(new Car("daewoo", "rus", 734));
-        this.carList.add(new Car("bmw", "de", 705));
-        this.carList.add(new Car("mercedes", "de", 333));
-        this.carList.add(new Car("kia", "kor", 222));
-        this.carList.add(new Car("Li", "chin", 133));
     }
 
     @GetMapping
     public String getCars(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
-        List<Car> carsToShow = carService.getCars(count, carList);
+        List<Car> carsToShow = carService.getCars(count);
         model.addAttribute("carList", carsToShow);
         return "cars";
     }
-
 }
-
-
-
-
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -3,12 +3,24 @@ package web.service;
 import org.springframework.stereotype.Service;
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CarService {
 
-    public List<Car> getCars(int count, List<Car> carList) {
+    private List<Car> carList;
+
+    public CarService() {
+        this.carList = new ArrayList<>();
+        this.carList.add(new Car("daewoo", "rus", 734));
+        this.carList.add(new Car("bmw", "de", 705));
+        this.carList.add(new Car("mercedes", "de", 333));
+        this.carList.add(new Car("kia", "kor", 222));
+        this.carList.add(new Car("Li", "chin", 133));
+    }
+
+    public List<Car> getCars(int count) {
         if (count >= carList.size()) {
             return carList;
         }
